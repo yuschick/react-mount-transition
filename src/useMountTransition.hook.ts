b208b9727530670ed928ddd487f88427211f10e4
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-12-09 09:27:53 +0200

    feat: setup main hook

---
+++ src/useMountTransition.hook.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+interface MountTransitionProps {
+    duration: number;
+    isShown: boolean;
+}
+
+export type MountTransitionHook = {
+    showTransitionChild: boolean;
+    withTransitionStyles: boolean;
+};
+
+export const useMountTransition = ({
+    isShown,
+    duration,
+}: MountTransitionProps): MountTransitionHook => {
+    const [hasTransitionedIn, setHasTransitionedIn] = useState<boolean>(false);
+
+    useEffect(() => {
+        let timeoutId: ReturnType<typeof setTimeout>;
+
+        if (isShown && !hasTransitionedIn) {
+            setHasTransitionedIn(true);
+        } else if (!isShown && hasTransitionedIn) {
+            timeoutId = setTimeout(() => setHasTransitionedIn(false), duration);
+        }
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [duration, isShown, hasTransitionedIn]);
+
+    return {
+        showTransitionChild: hasTransitionedIn || isShown,
+        withTransitionStyles: hasTransitionedIn && isShown,
+    };
+};
